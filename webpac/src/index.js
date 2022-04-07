67a974fb15ec1462161cafcec80833ad6ad66a08
snehanaik4737 <97469677+snehanaik4737@users.noreply.github.com>
2022-04-07 21:35:02 +0530

    webpack done

---
+++ webpac/src/index.js
@@ -0,0 +1,61 @@
+
+
+ import "./index.css"
+
+ import img from "../src/images/download (4).png";
+
+
+const logo =document.getElementById("logo");
+const image = document.createElement("img");
+image.src=img
+
+logo.append(image);
+
+
+let submit=document.querySelector(".submit");
+let notesElem=document.querySelector('.notes');
+let title=document.querySelector("#text");
+let desc=document.querySelector('#desc');
+let notes=JSON.parse(localStorage.getItem("notes"));
+if(notes){
+    notes.forEach(element => {
+        addNotes(element)
+    });
+}
+submit.addEventListener("click", (e)=>{
+    e.preventDefault();
+    addNotes()
+})
+function addNotes(obj) {
+    let card=document.createElement("div");
+    card.classList.add("card");
+    let titleval=title.value;
+    let descVal=desc.value;
+    if(obj){
+        titleval=obj.title;
+        descVal=obj.desc;
+    }
+    if(titleval){
+        card.innerHTML=`<h3>${titleval}</h3>
+                                    <p class="ptag">${descVal}</p>
+                             <button class="del">Delete</button>`;
+        notesElem.appendChild(card);
+        updateLs()
+    }
+    let del=card.querySelector(".del");
+    del.addEventListener('click', ()=>{
+        card.remove();
+        updateLs();
+    })
+}
+function updateLs() {
+    let card=document.querySelectorAll(".card");
+    let arr=[];
+    card.forEach(element => {
+        arr.push({
+            title:element.children[0].innerText,
+            desc:element.children[1].innerText
+        })
+    });
+    localStorage.setItem("notes", JSON.stringify(arr));
+}
---
+++ webpac/webpack.config.js
@@ -0,0 +1,23 @@
+const path =require("path");
+
+
+module.exports={
+    entry:"./src/index.js",
+    output:{
+        path:path.resolve(".","build"),
+        filename:"bundle.js"
+
+    },
+    mode:"development",
+    module: {
+        rules: [{ test: /\.css$/, use: ["style-loader","css-loader"] },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+            },
+          ],
+        },],
+      },
+};
