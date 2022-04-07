9bd8d398260c24d01014b72c21742624ad8c895c
snehanaik4737 <97469677+snehanaik4737@users.noreply.github.com>
2022-04-08 00:58:35 +0530

    ..

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -1,9 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css"
 function App() {
+  const os=["Android","Blckberry","iPhone","Windows Phone"];
+  const mn =["Samsung","HTC","Micromax","Apple"]
   return (
-   <h1>hello</h1>
+  <div className="App">
+    <div>
+      <h1>Mobile Operating System</h1>
+      <ul>
+        {os.map((e)=>{
+          return <li>{e}</li>
+        })}
+      </ul>
+
+    </div>
+    <div>
+      <h1>Mobile Manufacturers</h1>
+      <ul id="square">
+        {mn.map((e)=>{
+          return <li>{e}</li>
+        })}
+      </ul>
+
+    </div>
+
+  </div>
   );
 }
 
