0f17796aead3a778c7d3e188fd4f1947891f77a0
snehanaik4737 <97469677+snehanaik4737@users.noreply.github.com>
2022-04-08 21:13:48 +0530

    day2-assignment done

--- day-2/src/App.js
+++ day-2/src/App.js
@@ -1,10 +1,42 @@
 import "./App.css"
 function App() {
   const os=["Android","Blckberry","iPhone","Windows Phone"];
-  const mn =["Samsung","HTC","Micromax","Apple"]
+  const mn =[{name:"Samsung",style:"square"},
+      {name:"HTC",style:"square"},
+      {name:"Micromax",style:""},
+      {name:"Apple",style:"circle"}];
+
+
+ const links =[{name:"Services",link:""},
+ {
+  name:"Projects",link:""},
+  {
+name:"About",link:""
+  }
+ ]
+
+  
   return (
   <div className="App">
-    <div>
+
+  <div className="nav">
+     <div id="logo">
+        <Logo/>
+     </div>
+     <div id="links" >
+       {links.map((e)=>{
+         return <Links link={e}/>
+       })}
+     </div>
+
+<div>
+  <Button/>
+</div>
+    
+    </div>
+
+
+    <div className="bullets">
       <h1>Mobile Operating System</h1>
       <ul>
         {os.map((e)=>{
@@ -13,18 +45,37 @@ function App() {
       </ul>
 
     </div>
-    <div>
+    <div className="bullets">
       <h1>Mobile Manufacturers</h1>
-      <ul id="square">
+      <ul >
         {mn.map((e)=>{
-          return <li>{e}</li>
+          return <li className={e.style}>{e.name}</li>
         })}
       </ul>
 
     </div>
-
+ 
   </div>
   );
 }
+
+
+
+ function Logo(){
+   return <p>LOGOBAKERY</p>
+ }
+
+  function Links(props){
+   // console.log(props)
+    return <a id="href" href={props.link.link}>{props.link.name}</a>
+  }
+
+
+ function Button(){
+   return <button id="buttn">Contact</button>
+ }
+ 
+ 
+
 
 export default App;
